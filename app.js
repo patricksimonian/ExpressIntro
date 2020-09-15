65f1bb7ea9baa6332b58cb1a6746a6f2fad5137d
Patrick Simonian <patricksimonian@gmail.com>
2020-09-15 11:21:48 -0700

    updated the post people route to send json

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 3000
 
 var people = [{
   
-  firstName:'Andrew',
-  lastName:'Jun',
-  age:'23'
+  firstName: 'Andrew',
+  lastName: 'Jun',
+  age: '23'
   
 }]
 
@@ -25,25 +25,55 @@ app.get('/', (req, res) => {
   html += "</p>Age:<input type= 'number' name='age'>";
   html += "<input type='submit' value='submit'>";
   html += "</form>";
-  html += "</body>";
   res.send(html)
 })
 
 app.get('/health', (req, res) => {
   res.send('Server is healthy')
-})
+});
 
 app.get('/people', (req, res) => {
   res.send(JSON.stringify(people))
-})
+});
 
 app.post('/people', (req, res) => {
   people.push(req.body);
-  res.status(200) 
-  res.send(people);   
-})
+  res.status(200).json(req.body); 
+});
 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+
+
+
+
+const getCurrentRequest = (path) => ({
+  body: null,
+});
+
+function getCurrentResponse() {
+  return {
+    status: number => console.log(number),
+    send: msg => console.log(msg),
+    json: jsonString => this.send(JSON.stringify(jsonString)),
+  }
+};
+
+const get = (path, resReqCb) => {
+  const request = getCurrentRequest(path);
+  const response = getCurrentResponse();
+
+  resReqCb(request, response);
+}
+
+
+get('/foo', (req, res) => {
+  res.send('HELLO WORLD');
+});
+
+
+// nodemon
+// prettier
+// express.Router
